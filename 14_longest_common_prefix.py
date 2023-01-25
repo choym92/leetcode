9b8d002130aab8ab9ad9a5796ce89133f34edbe6
Paul Cho <choym92@gmail.com>
2023-01-25 16:08:08 -0500

    adding comments and explanations

--- 14_longest_common_prefix.py
+++ 14_longest_common_prefix.py
@@ -14,9 +14,20 @@ Explanation: There is no common prefix among the input strings.
 
 from typing import List
 
+"""
+Method:
+simutenously iterate through all of the strings that were given.
+One of the string can be shorter than the other.
+Iterating through every single characters where 
+
+O(n) and n being the number of characters.
+"""
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        # initialize initial result string
         result = ""
+
         for char_ in zip(*strs):
             if len(set(char_)) == 1:
                 result += char_[0]
@@ -35,3 +46,24 @@ checker.longestCommonPrefix(strs2)
 # Intuition:
 # compare each letter of strings and if match, add to an empty string until you hit a character that doesn't match
 # return the string added so far
+
+
+# Checking zip and *zip
+for i in zip([1,2,3],[4,5,6]):
+    print(i)
+# (1, 4)
+# (2, 5)
+# (3, 6)
+# take one char or value from each element treating them as if they are a list of on its own
+for i in zip(*['abc', '111', '222']):
+    print(i)
+
+# ('a', '1', '2')
+# ('b', '1', '2')
+# ('c', '1', '2')
+
+for i in zip(['abc', '111', '222']):
+    print(i)
+# ('abc',)
+# ('111',)
+# ('222',)
--- 20_valid_parenthesis.py
+++ 20_valid_parenthesis.py
@@ -20,19 +20,39 @@ Input: s = "(]"
 Output: false
 """
 
+"""
+Idea:
+We need to make sure we have open parenthesis first. 
+Cannot start with the closed parenthesis. 
+The ordering needs to be applied.
+We will be removing top and the beginning of the list.
+Therefore, we know that we need to utilize STACK
+Create a hashmap. 
+
+O(n) because we are having to go through the input character once. 
+O(n) of memory because we are using a stack and the stack can be the size of the input. 
+"""
 class Solution:
     def isValid(self, s: str) -> bool:
-        paren_dict = {')':'(', ']':'[', '}':'{'}
+        # hashmap and stack
+        hashmap = {')':'(', ']':'[', '}':'{'}
         stack = []
+
+        # go through each character at once of the string and 
         for char_ in s:
-            if char_ not in ']})':
+        # if the char is open paren add to the stack
+            if char_ not in ']})': 
                 stack.append(char_)
-            elif stack:
+            elif stack: # if closed paren and in value in stack there is char in stack
+                # pop and see if the value matches the according hashmap
                 x = stack.pop()
-                if x != paren_dict[char_]:
+                # if the ordering is off, return False
+                if x != hashmap[char_]:
                     return False
+            # if ordering is 
             else:
                 return False
+        # we check if the stack is empty else return False because it means the string contains closed parenthesis
         return True if len(stack) == 0 else False 
 
 checker = Solution()
